79df773316c8725e7839abfcd0b341997d1032c8
codingstorytime <codingstorytime@gmail.com>
2019-04-26 09:21:28 -0500

    added a message to the console

--- index.js
+++ index.js
@@ -2,8 +2,11 @@
 import './style.css';
 
 /*JavaScript uses var, let, const*/
-var speed;           //declaring variable called speed
-let speed;          //declaring variable called speed
-const speed = 20;  //declaring and initializing variable called speed
+var speed1;           //declaring variable called speed1
+let speed2;          //declaring variable called speed2
+const speed3 = 20;  //declaring and initializing variable called speed3
 
 
+console.log(speed3);
+
+console.log(`Kevin Ward was driving around a dirt track, the jeep started to break down, it was going ${speed3} miles an hour`);
